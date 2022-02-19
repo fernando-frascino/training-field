ff8b7f9109756e5a1f03f376769d03589ff364e0
fernando-frascino <fernandofrascino@yahoo.com.br>
2022-02-18 22:26:35 -0300

    quiz update

--- golang-gophercises-quiz/main.go
+++ golang-gophercises-quiz/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,10 +26,41 @@ func main() {
 		exit("Failed to parse the provided CSV file.")
 	}
 
-	fmt.Println(lines)
+	problems := readLines(lines)
+	correctCounter := 0
+
+	for i, problem := range problems {
+		fmt.Printf("Problem %d: %s = \n", i+1, problem.quetion)
+
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == problem.answer {
+			correctCounter++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d\n", correctCounter, len(problems))
+}
+
+func readLines(lines [][]string) []problem {
+	result := make([]problem, len(lines))
+
+	for i, line := range lines {
+		result[i] = problem{
+			quetion: line[0],
+			answer:  strings.TrimSpace(line[1]),
+		}
+	}
+
+	return result
 }
 
 func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
+}
+
+type problem struct {
+	quetion string
+	answer  string
 }
